2f00726dc32c49702de759e64fa5a3cb0eea3721
EmilEndresen <89249111+EmilEndresen@users.noreply.github.com>
2021-10-03 11:43:46 +0200

    more menu

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ closeButton.addEventListener("click", () => {
 openButton.addEventListener("click", () => {
     nav.classList.add('navigation-open');
 }); 
+
+
+let emailCollectorForm = document.getElementById("Email-Collector")
+emailCollectorForm.addEventListener("submit", event => {
+    event.preventDefault()
+
+    // use FormData to get the User's name and email    
+    let ourFormData = new FormData(event.target)
+    
+    let userFirstName = ourFormData.get("fname")
+    let userEmailAddress = ourFormData.get("email")
+        
+    let updatedHtmlContent = `
+        <h2>Thank you ${userFirstName}!</h2>
+
+        <p>You will get a confirmation of your booking sent to: ${userEmailAddress}</p>
+    `
+    
+    let updatedContentContainer = document.getElementById("Main-Text")
+    updatedContentContainer.innerHTML = updatedHtmlContent
+})
